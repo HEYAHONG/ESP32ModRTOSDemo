6565e7fbadf8752998f679d81711942c95e80b35
HEYAHONG <2229388563@qq.com>
2021-07-05 12:10:42 +0800

    从官方例程中添加mesh_netif

--- main/component.mk
+++ main/component.mk
@@ -2,15 +2,15 @@
 # "main" pseudo-component makefile.
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
-COMPONENT_ADD_INCLUDEDIRS := include
+COMPONENT_ADD_INCLUDEDIRS := include mesh_netif
 
-COMPONENT_OBJS = main.o  tftpd.o init.o wifinetwork.o
+COMPONENT_OBJS = main.o  tftpd.o init.o wifinetwork.o mesh_netif/mesh_netif.o
 
 
 #默认框架中为启用tftp,在此处启用
 COMPONENT_OBJS += tftp/tftp_server.o
 
-COMPONENT_SRCDIRS :=  ./
+COMPONENT_SRCDIRS :=  ./  mesh_netif
 
 #默认框架中为启用tftp,在此处启用
 COMPONENT_SRCDIRS += tftp
